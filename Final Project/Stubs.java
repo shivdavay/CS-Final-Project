2b56a033c9d545f4dcd301cd8ee0daa3ee27e056
Shiv Davay <davay.shiv@gmail.com>
2024-05-17 13:48:27 -0400

    stubs was added

---
+++ Final Project/Stubs.java
@@ -0,0 +1,23 @@
+// Freestyle Carrom Stubs
+// Shiv Davay and Ahan Banerjee
+// Period 1 - 5/20/2024
+
+
+
+import java.awt.*; // Used for basic AWT functionality in all the classes
+import java.awt.image.BufferedImage; // Used for creating and manipulating striker and discs images
+import javax.swing.*; // Used for creating graphical user interface components on Starting Panel Class
+import java.awt.event.*; // Used for handling events in the GUI components in all the classes
+import java.io.File; // Used for input-output operations in StartingPanel Class
+import java.io.IOException; // Used for input-output operations in StartingPanel Class
+import javax.sound.sampled.*; // Used for working with different songs in StartingPanel
+import java.awt.event.ActionEvent; // Used for handling actions performed in the GUI
+import java.awt.event.ActionListener; // Used for handling actions performed in the GUI
+import java.util.ArrayList; // Used for working with striker and various discs of all colors
+
+
+public class Stubs
+{
+
+
+}
