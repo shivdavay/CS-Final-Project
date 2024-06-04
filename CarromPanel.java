4e8ad461a5db75bc9630822b4f8555ae54c4d98d
Shiv Davay <davay.shiv@gmail.com>
2024-06-03 22:25:27 -0400

    instructions added

--- CarromPanel.java
+++ CarromPanel.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import javax.swing.*;
 
--- CarromPieces.java
+++ CarromPieces.java
@@ -1,7 +1,5 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import javax.swing.*;
+
 // this is an interface
 
 /**
